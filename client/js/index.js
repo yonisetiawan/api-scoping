8c19cfa1f052614eeeeb773e07d80e0b965c3a09
yonisetiawan <yonisetiawan1990@gmail.com>
2017-02-19 14:03:15 +0700

    Change Title Form URL DONE

--- client/js/index.js
+++ client/js/index.js
@@ -1,3 +1,4 @@
+const meals = ["salad","vegans","apples","lemon","grilling","dinners","checken","rice","gluten"]
 
 function dataHeading() {
   if(document.querySelector("div.ui h1").innerHTML=="Maintain State"){
@@ -5,5 +6,27 @@ function dataHeading() {
   }else{
     document.querySelector("div.ui h1").innerHTML = "Maintain State"
   }
+}
+
+function skill() {
+    document.getElementById("listbtn").innerHTML = `<button onclick="dataURL()" data="Change Data Heading" class="ui teal button">Skill Submit</button>`
+}
+
+function pagehome() {
+  document.getElementById("listbtn").innerHTML =   `<button onclick="dataHeading()" data="Change Data Heading" class="ui teal button">Data-Heading</button>`
+}
+
+function pagemeals() {
+  document.getElementById("listbtn").innerHTML =   `<button onclick="dataMeals()" data="Change Data Heading" class="ui teal button">Data-Meals</button>`
+}
+
+function dataURL() {
+  if(document.querySelector("div.ui h1").innerHTML=="Maintain State"){
+    document.querySelector("div.ui h1").innerHTML = document.URL
+  }else{
+    document.querySelector("div.ui h1").innerHTML = "Maintain State"
+  }
+}
+function dataMeals() {
 
 }
--- server/routes/user.js
+++
@@ -1,8 +0,0 @@
-// const express = require('express');
-// const router = express.Router()
-// const Users = require('../controllers/users');
-// const DataUsers = require('../helpers/user');
-//
-//
-//
-// module.exports = router
